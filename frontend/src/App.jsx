f43fa3894991fb52ec978d98358fbc5521cb8637
Kayo Brenno <Kbreno822@gmail.com>
2026-05-29 22:11:14 -0300

    fix: corrige rota do dashboard e adiciona rota de professores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Layout from "./components/Layout"
 
 import AlunosPage from "./pages/AlunosPage"
-import DashboardPage2 from "./pages/DashboardPage.jsx"
+import DashboardPage from "./pages/DashboardPage.jsx"
 import DisciplinasPage from "./pages/DisciplinasPage"
 import HistoricoPage from "./pages/HistoricoPage"
+import ProfessoresPage from "./pages/ProfessorPage.jsx";
 
 export default function App() {
     return (
@@ -12,8 +13,9 @@ export default function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Navigate to="/alunos" replace />} />
-                    <Route path="/dashboard" element={<DashboardPage2 />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
                     <Route path="/alunos" element={<AlunosPage />} />
+                    <Route path="/professores" element={<ProfessoresPage />} />
                     <Route path="/disciplinas" element={<DisciplinasPage />} />
                     <Route path="/historico" element={<HistoricoPage />} />
                 </Routes>
